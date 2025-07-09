2cfdd7de72530ecaa65798816319c6e1bcf563e8
andersongames <andersonpgames@gmail.com>
2025-07-09 09:32:34 -0300

    feat: list rooms

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,9 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
+import { ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { Badge } from '@/components/ui/badge';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 type GetRoomsAPIResponse = Array<{
   id: string;
   name: string;
+  questionsCount: number;
+  createdAt: string;
 }>;
 
 export function CreateRoom() {
@@ -17,20 +28,50 @@ export function CreateRoom() {
   });
 
   return (
-    <div>
-      <div>Create Room</div>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <div />
 
-      {isLoading && <p>Loading...</p>}
+          <Card>
+            <CardHeader>
+              <CardTitle>Recent Rooms</CardTitle>
+              <CardDescription>
+                Quick access to recently created rooms
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-3">
+              {data?.map((room) => {
+                return (
+                  <Link
+                    className="flex cursor-pointer items-center justify-between rounded-lg border p-3 hover:bg-accent"
+                    key={room.id}
+                    to={`/rooms/${room.id}`}
+                  >
+                    <div className="flex flex-1 flex-col gap-1">
+                      <h3 className="font-medium">{room.name}</h3>
 
-      <div className='flex flex-col gap-1'>
-        {data?.map((room) => {
-            return <Link key={room.id} to={`/room/${room.id}`}>{room.name}</Link>;
-          })}
+                      <div className="flex items-center gap-2">
+                        <Badge className="text-xs" variant="secondary">
+                          {room.createdAt}
+                        </Badge>
+                        <Badge className="text-xs" variant="secondary">
+                          {room.questionsCount} perguntas(s)
+                        </Badge>
+                      </div>
+                    </div>
+
+                    <span className="flex items-center gap-1 text-sm">
+                      Join
+                      <ArrowRight className="size-3" />
+                    </span>
+                  </Link>
+                );
+              })}
+            </CardContent>
+          </Card>
+        </div>
       </div>
-
-      <Link className="underline" to="/room">
-        Acessar sala
-      </Link>
     </div>
   );
 }
