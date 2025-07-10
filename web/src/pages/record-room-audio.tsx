ff72d5cf65f7fd98a4b3a19f9396a8f9542da46f
andersongames <andersonpgames@gmail.com>
2025-07-10 11:51:25 -0300

    feat: record audio

---
+++ web/src/pages/record-room-audio.tsx
@@ -0,0 +1,102 @@
+import { useRef, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+
+type AudioParams = {
+  roomId: string;
+};
+
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === 'function' &&
+  typeof window.MediaRecorder === 'function';
+
+export function RecordRoomAudio() {
+  const params = useParams<AudioParams>();
+  const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef<MediaRecorder | null>(null);
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+
+    formData.append('file', audio, 'audio.webm');
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    const result = await response.json();
+
+    console.log(result);
+  }
+
+  function stopRecording() {
+    setIsRecording(false);
+
+    if (recorder.current && recorder.current.state !== 'inactive') {
+      recorder.current.stop();
+    }
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert('Your browser does not support recording.');
+      return;
+    }
+
+    setIsRecording(true);
+
+    try {
+      const audio = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          echoCancellation: true,
+          noiseSuppression: true,
+          sampleRate: 44_100,
+        },
+      });
+
+      recorder.current = new MediaRecorder(audio, {
+        mimeType: 'audio/webm',
+        audioBitsPerSecond: 64_000,
+      });
+
+      recorder.current.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          uploadAudio(event.data);
+          console.log(event.data);
+        }
+      };
+
+      recorder.current.onstart = (event) => {
+        console.log('Recording started');
+      };
+
+      recorder.current.onstop = (event) => {
+        console.log('Recording stoped');
+      };
+
+      recorder.current.start();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-3">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Pause Recording</Button>
+      ) : (
+        <Button onClick={startRecording}>Record audio</Button>
+      )}
+      {isRecording ? <p>Recording...</p> : <p>Paused</p>}
+    </div>
+  );
+}
